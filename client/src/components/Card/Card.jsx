c7342abd53ba90e427d5342d20631af0479ab88f
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-01-21 01:00:59 +0530

    Connected Strapi with Frontend

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,20 +1,31 @@
 import './Card.scss'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const Card = ({ item }) => {
+
     return (
         <Link className='link' to={`/product/${item.id}`}>
             <div className='card'>
                 <div className="image">
-                    {item.isNew && <span>New Season</span>}
-                    <img src={item.img} alt="" className="mainImage" />
-                    <img src={item.img1} alt="" className="secondImage" />
+                    {item?.attributes.isNew && <span>New Season</span>}
+                    <img src={
+                        import.meta.env.VITE_REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url
+                    }
+                        alt=""
+                        className="mainImage"
+                    />
+                    <img src={
+                        import.meta.env.VITE_REACT_APP_UPLOAD_URL + item.attributes?.img1?.data?.attributes?.url
+                    }
+                        alt=""
+                        className="secondImage"
+                    />
                 </div>
 
-                <h2>{item.title}</h2>
+                <h2>{item?.attributes.title}</h2>
                 <div className="prices">
-                    <h3>${item.oldPrice}</h3>
-                    <h3>${item.price}</h3>
+                    <h3>${item?.attributes.oldPrice || item?.attributes.price + 20}</h3>
+                    <h3>${item?.attributes.price}</h3>
                 </div>
             </div>
         </Link>
